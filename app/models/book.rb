d6931321c702f0558b9131f632e9313ab8c6b58c
Timothy Alexis Vass <timvas@bredband2.se>
2023-07-03 20:39:52 +0200

    Update new book form

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,19 @@
 class Book < ApplicationRecord
   enum book_type: { fiction: 0, factual: 1 }
-  enum category: { science: 0, humanity: 1, technology: 2, arts: 3, worldly: 4 }
+  
+  FICTION_CATEGORIES = %w[fantasy feelgood other].freeze
+  FACTUAL_CATEGORIES = %w[science humanities technology arts worldly].freeze
+
+  validates :category, inclusion: { in: FICTION_CATEGORIES, if: :fiction? }
+  validates :category, inclusion: { in: FACTUAL_CATEGORIES, if: :factual? }
+
+  def self.fiction_categories
+    FICTION_CATEGORIES
+  end
+
+  def self.factual_categories
+    FACTUAL_CATEGORIES
+  end
 
   has_many :chapters
 end
--- db/migrate/20230703153553_create_books.rb
+++ db/migrate/20230703153553_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
       t.integer :type
-      t.integer :category
+      t.string :category
       t.string :subcategory
       t.string :title
 
